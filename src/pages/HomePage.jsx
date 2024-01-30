f7792ca7e253e91bc55c47154e2b7e607c016a45
selunders <sethlunders@gmail.com>
2024-01-29 22:21:27 -0800

    Add social links to home screen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,61 @@
-import * as styles from './HomePage.module.css'
+import { For } from "solid-js";
+import * as styles from "./HomePage.module.css";
+
+import { FaBrandsGoodreadsG, FaBrandsInstagram, FaBrandsPinterest, FaBrandsSpotify } from 'solid-icons/fa';
+import { IoMailOutline } from 'solid-icons/io';
 
 export default function HomePage() {
     return (
         <div class={styles.homepage}>
-
+            <img
+                src="/img/home_anton-sharov-unsplash-min.webp"
+                alt="Wooden walkway leading to ocean beach."
+            />
+            <div class={styles.links}>
+                <ul>
+                    <For each={myLinks}>
+                        {link => (
+                            <li>
+                                <a class={link.class ?? null} href={link.href} target="_blank" aria-label={link.ariaLabel}>
+                                    {link.display}
+                                </a>
+                            </li>
+                        )}
+                    </For>
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
+
+const myLinks = [
+    {
+        href: "https://www.pinterest.com/authorjamiesheehan/",
+        class: styles.pinterestLink,
+        ariaLabel: "Visit my Pinterest",
+        display: () => <FaBrandsPinterest />
+    },
+    {
+        href: "https://open.spotify.com/user/jamiedanae",
+        class: styles.spotifyLink,
+        ariaLabel: "Follow me on Spotify",
+        display: () => <FaBrandsSpotify />
+    },
+    {
+        href: "https://www.goodreads.com/user/show/44081734-jamie-danae",
+        ariaLabel: "Check out my Goodreads",
+        display: () => <FaBrandsGoodreadsG />
+    },
+    {
+        href: "https://www.instagram.com/authorjamiesheehan",
+        class: styles.igLink,
+        ariaLabel: "Follow me on Instagram",
+        display: () => <FaBrandsInstagram />
+    },
+    {
+        href: "mailto:authorjamiesheehan@gmail.com",
+        class: styles.mailLink,
+        ariaLabel: "Send me an email",
+        display: () => <IoMailOutline />
+    }
+]
