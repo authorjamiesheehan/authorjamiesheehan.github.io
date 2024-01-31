2869605f709cd04b19a335b76c63a1e6f968ca24
selunders <sethlunders@gmail.com>
2024-01-30 19:31:36 -0800

    Add Books page

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import { Route, Router } from '@solidjs/router'
 import App from './AppLayout'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
+import BookPage from './pages/BookPage'
 
 const root = document.getElementById('root')
 
@@ -13,5 +14,6 @@ render(() => (
     <Router root={App}>
         <Route path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/books" component={BookPage} />
     </Router>
 ), root)
