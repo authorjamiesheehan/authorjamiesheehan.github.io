0dfb016b560648f5a765c4c3fb2c14f25f0fdc07
selunders <sethlunders@gmail.com>
2024-01-30 18:24:41 -0800

    Add quote to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import * as styles from "./HomePage.module.css";
 import { FaBrandsGoodreadsG, FaBrandsInstagram, FaBrandsPinterest, FaBrandsSpotify } from 'solid-icons/fa';
 import { IoMailOutline } from 'solid-icons/io';
 
+
+const footerQuote = {
+    quote: "First, I do not sit down at my desk to put into verse something that is already clear in my mind. If it were clear in my mind, I should have no incentive or need to write about it. We do not write in order to be understood; we write in order to understand.",
+    attribution: "Cecil Day Lewis"
+}
+
+
 export default function HomePage() {
 
     window.CustomSubstackWidget = {
@@ -30,29 +37,35 @@ export default function HomePage() {
     })
 
     return (
-        <div class={styles.homepage} >
-            <img
-                src="/img/home_anton-sharov-unsplash-min.webp"
-                alt="Wooden walkway leading to ocean beach."
-            />
-            <div class={styles.links}>
-                <ul>
-                    <For each={myLinks}>
-                        {link => (
-                            <li>
-                                <a class={link.class ?? null} href={link.href} target="_blank" aria-label={link.ariaLabel}>
-                                    {link.display}
-                                </a>
-                            </li>
-                        )}
-                    </For>
-                </ul>
-                <div>
-                    <p class={styles.substackLink}>Subscribe to my newsletter:</p>
-                    <div id="custom-substack-embed"></div>
+        <>
+            <div class={`${styles.homepage} ${styles.mainContent}`} >
+                <img
+                    src="/img/home_anton-sharov-unsplash-min.webp"
+                    alt="Wooden walkway leading to ocean beach."
+                />
+                <div class={styles.links}>
+                    <ul>
+                        <For each={myLinks}>
+                            {link => (
+                                <li>
+                                    <a class={link.class ?? null} href={link.href} target="_blank" aria-label={link.ariaLabel}>
+                                        {link.display}
+                                    </a>
+                                </li>
+                            )}
+                        </For>
+                    </ul>
+                    <div>
+                        <p class={styles.substackLink}>Subscribe to my newsletter:</p>
+                        <div id="custom-substack-embed"></div>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class={styles.quoteBlock}>
+                <p>"{footerQuote.quote}"</p>
+                <p>— {footerQuote.attribution}</p>
+            </div>
+        </>
     );
 }
 
