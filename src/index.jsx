dcb1b12aaf23edc640f774ad0da0827927f1a147
selunders <sethlunders@gmail.com>
2024-01-30 21:01:40 -0800

    Update to use HashRouter for GH

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 import { render } from 'solid-js/web'
 
 import './index.css'
-import { Route, Router } from '@solidjs/router'
+import { Route, HashRouter } from '@solidjs/router'
 import App from './AppLayout'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
@@ -12,10 +12,10 @@ import ContactPage from './pages/ContactPage'
 const root = document.getElementById('root')
 
 render(() => (
-    <Router root={App}>
+    <HashRouter root={App}>
         <Route path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/books" component={BookPage} />
         <Route path="/contact" component={ContactPage} />
-    </Router>
+    </HashRouter>
 ), root)
